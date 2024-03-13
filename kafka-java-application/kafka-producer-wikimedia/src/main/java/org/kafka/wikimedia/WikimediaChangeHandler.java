69d9bf1db70a283946c951c16eaa147fd2022370
Rohan Aggarwal <aggarwal.rohan17@gmail.com>
2024-03-13 10:29:22 +0400

    Kafka producers to stream wikimedia

---
+++ kafka-java-application/kafka-producer-wikimedia/src/main/java/org/kafka/wikimedia/WikimediaChangeHandler.java
@@ -0,0 +1,47 @@
+package org.kafka.wikimedia;
+
+
+import com.launchdarkly.eventsource.MessageEvent;
+import com.launchdarkly.eventsource.background.BackgroundEventHandler;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WikimediaChangeHandler implements BackgroundEventHandler {
+
+    KafkaProducer<String, String> kafkaProducer;
+    String topic;
+
+    private static Logger log = LoggerFactory.getLogger(WikimediaChangeHandler.class.getSimpleName());
+
+    public WikimediaChangeHandler(KafkaProducer<String,String> producer, String topic) {
+        this.kafkaProducer = producer;
+        this.topic = topic;
+    }
+    @Override
+    public void onOpen() {
+        // nothing here
+    }
+
+    @Override
+    public void onClosed() {
+        kafkaProducer.close();
+    }
+
+    @Override
+    public void onMessage(String event, MessageEvent messageEvent) throws Exception {
+        log.info(messageEvent.getData());
+        kafkaProducer.send(new ProducerRecord<>(topic, messageEvent.getData()));
+    }
+
+    @Override
+    public void onComment(String comment) {
+        //  nothing here
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        log.error("Error in stream reading", t);
+    }
+}
--- kafka-java-application/kafka-producer-wikimedia/src/main/java/org/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-java-application/kafka-producer-wikimedia/src/main/java/org/kafka/wikimedia/WikimediaChangesProducer.java
@@ -1,7 +1,59 @@
 package org.kafka.wikimedia;
 
+import com.launchdarkly.eventsource.EventSource;
+import com.launchdarkly.eventsource.ReadyState;
+import com.launchdarkly.eventsource.StreamEvent;
+import com.launchdarkly.eventsource.StreamException;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 public class WikimediaChangesProducer {
-    public static void main(String[] args) {
+    private static Logger log = LoggerFactory.getLogger(WikimediaChangesProducer.class.getSimpleName());
+    public static void main(String[] args) throws StreamException, InterruptedException {
+
+        String bootStrapServers = "127.0.0.1:9092";
+
+        // Create producer properties
+        Properties properties = new Properties();
+        // Connect to localhost
+        properties.setProperty("bootstrap.servers", bootStrapServers);
+
+        // set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer",StringSerializer.class.getName());
+
+        // create a producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String topic = "wikimedia.recentchange";
+
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(URI.create(url));
+        EventSource eventSource = builder.build();
+
+        if(eventSource.getState().equals(ReadyState.SHUTDOWN) || eventSource.getState().equals(ReadyState.CLOSED)) {
+            producer.close();
+        } else {
+            eventSource.messages().forEach(message -> {
+                log.info(message.getData());
+                producer.send(new ProducerRecord<>(topic, message.getData()));
+            });
+
+        }
+        eventSource.start();
+
+        // We produce for 2 minutes and block the program until then
+        TimeUnit.MINUTES.sleep(2);
+
+
+
 
     }
 }
